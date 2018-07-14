d7f0af347bc0d0f775bafc24bb274d125b3f7945
ernestognw <ernestognw@gmail.com>
2018-07-13 23:49:45 -0500

    Clase 15

--- Assets/Script/DeadZone.cs
+++ Assets/Script/DeadZone.cs
@@ -1,15 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class DeadZone : MonoBehaviour {
 
-  private void OnCollisionEnter2D (Collision2D collision){
-    Debug.Log("Colisión");
-  }
+  public Text scorePlayerText;
+  public Text scoreEnemyText;
+
+  int scorePlayerQuantity;
+  int scoreEnemyQuantity;
 
   private void OnTriggerEnter2D (Collider2D collision){
-    Debug.Log("Trigger");
+    if (gameObject.tag == "Left") {
+      scoreEnemyQuantity++;
+      UpdateScoreLabel(scoreEnemyText, scoreEnemyQuantity);
+    } else if (gameObject.tag == "Right") {
+      scorePlayerQuantity++;
+      UpdateScoreLabel(scorePlayerText, scorePlayerQuantity);      
+    }
+  }
+
+  void UpdateScoreLabel(Text label, int score) {
+    label.text = score.ToString();
   }
 }
